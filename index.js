b93ef588455a7d1307dcc03c0053a1a8fb68f719
showmittro <showmittrobiswas2000@gmail.com>
2022-05-26 02:23:12 +0600

    parts id add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -16,13 +16,34 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwxsr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         const globalPartsCollection = client.db('globalParts').collection('parts')
 
+        app.get('/parts', async (req, res) => {
+            const query = {};
+            const cursor = globalPartsCollection.find(query);
+            const parts = await cursor.toArray();
+
+            res.send(parts);
+
+        })
+        app.get('/parts/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const parts = await globalPartsCollection.findOne(query);
+            res.send(parts);
+        });
+        // app.get('/parts/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const parts = await globalPartsCollection.findOne(query);
+        //     res.send(parts);
+        // });
+
     }
-    finally{
+    finally {
 
     }
 }
